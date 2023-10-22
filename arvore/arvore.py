1d900c4c3f13b8f641ceb4da1241c7bbef6c937d
Yuri Dias Pereira <yuristardias@hotmail.com>
2023-10-21 22:10:34 -0300

    update

--- arvore/arvore.py
+++ arvore/arvore.py
@@ -30,5 +30,78 @@ class Arvore:
             else:
                 self.__inserir(referencia.no_esquerdo, no)
 
+    def buscar(self, no_busca):
+        if self.raiz is None:
+            return None
+        else:
+            return self.__buscar(self.raiz, no_busca)
+
+    def __buscar(self, referencia, no_busca):
+        if referencia.valor == no_busca.valor:
+            return referencia
+        elif referencia.peso() < no_busca.peso():
+            if referencia.no_direito is None:
+                raise ValueError("Elemento não encontrado")
+            else:
+                return self.__buscar(referencia.no_direito, no_busca)
+        else:
+            if referencia.no_esquerdo is None:
+                raise ValueError("Elemento não encontrado")
+            else:
+                return self.__buscar(referencia.no_esquerdo, no_busca)
+
+    def em_ordem(self):
+        self.__em_ordem(self.raiz)
+
+    def __em_ordem(self, referencia):
+        if referencia.no_esquerdo != None:
+            self.__em_ordem(referencia.no_esquerdo)
+            print(referencia.valor.__str__())
+            if referencia.no_direito != None:
+                self.__em_ordem(referencia.no_direito)
+        else:
+            print(referencia.valor.__str__())
+            if referencia.no_direito != None:
+                self.__em_ordem(referencia.no_direito)
+
+    def pre_ordem(self):
+        self.__pre_ordem(self.raiz)
+
+    def __pre_ordem(self, referencia):
+        print(referencia.valor.__str__())
+        if referencia.no_esquerdo != None:
+            self.__pre_ordem(referencia.no_esquerdo)
+            if referencia.no_direito != None:
+                self.__pre_ordem(referencia.no_direito)
+        else:
+            if referencia.no_direito != None:
+                self.__pre_ordem(referencia.no_direito)
+
+    def pos_ordem(self):
+        self.__pos_ordem(self.raiz)
+
+    def __pos_ordem(self, referencia):
+        if referencia.no_esquerdo != None:
+            self.__pos_ordem(referencia.no_esquerdo)
+            if referencia.no_direito != None:
+                self.__pre_ordem(referencia.no_direito)
+            print(referencia.valor.__str__())
+        else:
+            if referencia.no_direito != None:
+                self.__pre_ordem(referencia.no_direito)
+                print(referencia.valor.__str__())
+            else:
+                print(referencia.valor.__str__())
+
+    def altura(self):
+        return self.__altura(self.raiz)
+
+    def __altura(self, referencia):
+        if referencia is None:
+            return -1
+        altura_esquerda = self.__altura(referencia.no_esquerdo)
+        altura_direita = self.__altura(referencia.no_direito)
+        return (altura_esquerda + 1) if altura_esquerda > altura_direita else altura_direita + 1
+
     def __str__(self):
         return "[(X)]" if self.raiz is None else f"[({self.raiz.__str__()})]"
